6318502d8d46dc3f61cddc1e33e2a35f5fbab7b6
nkurudavid <nkurudavid44@gmail.com>
2025-11-23 22:34:17 +0200

    new updates

--- api/apps/usr/admin.py
+++ api/apps/usr/admin.py
@@ -6,51 +6,22 @@ from apps.usr.models import User
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    """Professional admin panel for custom User model."""
-
-    # Fields to show in list view
-    list_display = (
-        "id",
-        "first_name",
-        "last_name",
-        "email",
-        "role",
-        "gender",
-        "is_active",
-        "is_staff",
-        "created_at",
-    )
-    list_display_links = ("id", "email")
+    list_display = ("email", "first_name", "last_name", "role", "gender", "is_active", "is_staff", "created_at",)
+    list_display_links = ("email",)
     list_filter = ("role", "gender", "is_active", "is_staff", "created_at")
-
-    # Search by name or email
     search_fields = ("email", "first_name", "last_name")
-
-    # Order by newest users first
     ordering = ("-created_at",)
-
     readonly_fields = ("created_at",)
-
-    # Remove username since your model removed it
+    
     fieldsets = (
-        (_("Login Credentials"), {
-            "fields": ("email", "password")
-        }),
-        (_("Personal Info"), {
-            "fields": ("first_name", "last_name", "gender")
-        }),
-        (_("Role & Permissions"), {
-            "fields": ("role", "is_active", "is_staff", "is_superuser", "groups", "user_permissions")
-        }),
-        (_("Important Dates"), {
-            "fields": ("last_login", "created_at")
-        }),
+        ("Login Credentials", {"fields": ("email", "password")}),
+        ("Personal Info", {"fields": ("first_name", "last_name", "gender")}),
+        ("Role & Permissions", {"fields": ("role", "is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important Dates", { "fields": ("last_login", "created_at")}),
     )
-
-    # For creating new users in admin
     add_fieldsets = (
         (None, {
             "classes": ("wide",),
-            "fields": ("email", "first_name", "last_name", "gender", "role", "password1", "password2"),
+            "fields": ("first_name", "last_name", "gender", "role", "email", "password1", "password2"),
         }),
     )
