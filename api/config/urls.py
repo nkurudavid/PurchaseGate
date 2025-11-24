4102150bb30a2d615c097f96aa0a1ed13822e633
nkurudavid <nkurudavid44@gmail.com>
2025-11-24 12:24:37 +0200

    media and static config

--- api/config/urls.py
+++ api/config/urls.py
@@ -1,6 +1,11 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
 
 # Home view for the index page
 def home(request):
@@ -13,3 +18,8 @@ urlpatterns = [
     path('purchases/', include('apps.purchases.urls')),
     path('', home, name='home'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
