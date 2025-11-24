cad43cb463f1adbe9d62178c6a565a3d23375db8
nkurudavid <nkurudavid44@gmail.com>
2025-11-24 14:15:59 +0200

    purchase url implemented

--- api/apps/purchases/urls.py
+++ api/apps/purchases/urls.py
@@ -1,14 +1,22 @@
-from django.urls import path
+from django.urls import include, path
 from apps.purchases import views
 from rest_framework.routers import DefaultRouter
+from apps.purchases.views import (
+    StaffPurchaseRequestViewSet, 
+    ApproverPurchaseRequestViewSet, 
+    FinancePurchaseRequestViewSet, 
+    ApprovalStepViewSet, 
+    FinanceNoteViewSet
+)
 
 router = DefaultRouter()
+router.register("staff/requests", StaffPurchaseRequestViewSet, basename="staff-requests")
+router.register("approver/requests", ApproverPurchaseRequestViewSet, basename="approver-requests")
+router.register("finance/requests", FinancePurchaseRequestViewSet, basename="finance-requests")
+router.register("approval_steps", ApprovalStepViewSet, basename="approval-steps")
+router.register("finance_notes", FinanceNoteViewSet, basename="finance-notes")
 
-# router.register(r'users', views.UserViewSet, basename='users')
 
 urlpatterns = [
-    # path('login/', views.LoginView.as_view(), name='login'),
-    # path('register/', views.RegisterView.as_view(), name='register'),
+    path("", include(router.urls)),
 ]
-
-urlpatterns += router.urls
