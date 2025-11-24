1bcc71e659c9647b316e59df9e5bb6e1659f1481
nkurudavid <nkurudavid44@gmail.com>
2025-11-24 15:54:57 +0200

    new updates on finance note view functionality

--- api/apps/purchases/views.py
+++ api/apps/purchases/views.py
@@ -1,5 +1,5 @@
 import uuid
-from rest_framework import viewsets, status
+from rest_framework import viewsets, mixins, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -107,14 +107,44 @@ class ApprovalStepViewSet(viewsets.GenericViewSet):
 
 
 
-class FinanceNoteViewSet(viewsets.ModelViewSet):
+class FinanceNoteViewSet(viewsets.GenericViewSet,
+                         mixins.CreateModelMixin,
+                         mixins.UpdateModelMixin,
+                         mixins.DestroyModelMixin):
     permission_classes = [IsAuthenticated, IsFinanceOfficer]
     serializer_class = FinanceNoteSerializer
-    queryset = FinanceNote.objects.none()
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        request_id = self.kwargs.get('request_pk')
+        if request_id:
+            queryset = queryset.filter(purchase_request=request_id)
+        return queryset
 
-    # block list + retrieve (write-only)
-    def list(self, request, *args, **kwargs):
-        return Response({"detail": "Not allowed."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response({
+            'message': 'Finance note added successfully!',
+            'data': serializer.data
+        }, status=status.HTTP_201_CREATED, headers=headers)
 
-    def retrieve(self, request, *args, **kwargs):
-        return Response({"detail": "Not allowed."}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response({
+            'message': 'Finance note updated successfully!',
+            'data': serializer.data
+        }, status=status.HTTP_200_OK)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({
+            'message': 'Finance note deleted successfully!'
+        }, status=status.HTTP_204_NO_CONTENT)
