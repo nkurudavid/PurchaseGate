5aac837b69724f47c141f6d4d0ee7a444b58be92
nkurudavid <nkurudavid44@gmail.com>
2025-11-24 21:48:20 +0200

    purchase url new updates, nested router implemented

--- api/apps/purchases/urls.py
+++ api/apps/purchases/urls.py
@@ -1,18 +1,22 @@
 from django.urls import include, path
 from apps.purchases import views
-from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 from apps.purchases.views import (
     PurchaseRequestViewSet,
     ApprovalStepViewSet, 
     FinanceNoteViewSet
 )
 
-router = DefaultRouter()
+router = routers.SimpleRouter()
 router.register("requests", PurchaseRequestViewSet, basename="requests")
-router.register("approval_steps", ApprovalStepViewSet, basename="approval-steps")
-router.register("finance_notes", FinanceNoteViewSet, basename="finance-notes")
+
+# Nested router under "requests/<id>/"
+requests_router = routers.NestedSimpleRouter(router, "requests", lookup="request")
+requests_router.register("approval_steps", ApprovalStepViewSet, basename="approval-steps")
+requests_router.register("finance_notes", FinanceNoteViewSet, basename="finance-notes")
 
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("", include(requests_router.urls)),
 ]
