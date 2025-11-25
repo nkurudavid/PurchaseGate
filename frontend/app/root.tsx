f13490ea5c70687667903884d98e02ba3fda87b4
nkurudavid <nkurudavid44@gmail.com>
2025-11-25 21:42:13 +0200

    new updates

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -5,14 +5,14 @@ import './app.css';
 
 export function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
+      <body suppressHydrationWarning>
         {children}
         <ScrollRestoration />
         <Scripts />
