87d2e021f4b919dd1b92502014b5d69747211a4d
nkurudavid <nkurudavid44@gmail.com>
2025-11-29 14:05:12 +0200

    ApprovalPolicy api introduced

--- api/apps/purchases/urls.py
+++ api/apps/purchases/urls.py
@@ -3,11 +3,13 @@ from apps.purchases import views
 from rest_framework_nested import routers
 from apps.purchases.views import (
     PurchaseRequestViewSet,
-    FinanceNoteViewSet
+    FinanceNoteViewSet,
+    ApprovalPolicyViewSet
 )
 
 router = routers.SimpleRouter()
 router.register("requests", PurchaseRequestViewSet, basename="requests")
+router.register("approval_policies", ApprovalPolicyViewSet, basename="approvalpolicy")
 
 # Nested router under "requests/<id>/"
 requests_router = routers.NestedSimpleRouter(router, "requests", lookup="request")
